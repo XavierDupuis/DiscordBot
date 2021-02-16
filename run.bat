9e35a467ec7fb124b3c834b58b46b52324e11d04
XavierDupuis <XavierDupuis@users.noreply.github.com>
2021-02-16 16:30:22 -0500

    python pip upgrade

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 @echo off
 mode con:cols=36 lines=11
 if not exist "keys" (mkdir keys && echo. 2>keys/DiscordToken.txt)
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 python main.py
